affc86d1e1ca163b24dff86dd4aca063f4fed2b0
yuto1017 <yuto1017.oyama@gmail.com>
2024-03-04 00:37:06 +0900

    チャットルーム作成機能の実装

--- db/migrate/20240303135505_create_rooms.rb
+++ db/migrate/20240303135505_create_rooms.rb
@@ -1,8 +1,7 @@
 class CreateRooms < ActiveRecord::Migration[7.0]
   def change
     create_table :rooms do |t|
-      t.references :room, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
+      t.string :name, null: false
       t.timestamps
     end
   end
