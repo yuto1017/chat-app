edc4671a83f2aa8351f8576ea67609cc5345f232
yuto1017 <yuto1017.oyama@gmail.com>
2024-03-04 22:46:07 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'messages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  root to: "messages#index"
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
